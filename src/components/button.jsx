de6635f474387eb9f14227f914c44386d5bf15e9
annydrn <andrea.duran.cabana@gmail.com>
2022-09-19 20:38:14 -0300

    agregue el 3 componente para separar la funcion del boton

---
+++ src/components/button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const button = () => {
+  return (
+    <div>
+        <button 
+    className='card_btn' 
+    style={backgroundObj}
+    onClick={getRandomAll}> Next  </button>
+    </div>
+  )
+}
+
+export default button
